4bfd0d70eea7ae91505d7874978958534186c05c
SheikhAbdullahSoft <audiobook.urdunovels@gmail.com>
2024-06-07 06:44:57 +0500

    Js data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("welcome to JS"); // we are using Node.js not browser
+
+/*
+console.log(3 +
+     3);
+console.log(3 + 3); console.log("welcome to JS") // not good for code readability
+*/
+
+// resources to explore JS
+// https://tc39.es/ecma262/
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+// Primitive Data Types
+let num = 124; // number
+let num2 = 753566799764324456885677n; // BigInt
+let num3 = BigInt(8654579975433567); // BigInt
+let text = "Pakistan"; // string
+let text2 = ""; // empty string
+let isVisible = true; // boolean
+let isNotVisible = false; // boolean
+let area; // undefined - not declared or defined yet
+let temprature = null; // to strictly declare that the data is emmpty not undefined
+const sym1 = Symbol(); // symbol for uniquness
+const sym2 = Symbol("foo"); // symbol
+const sym3 = Symbol("foo"); // symbol
+// here: sym2 !== sym3
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
